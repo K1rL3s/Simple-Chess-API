a3564c4c52fef85721f5c1ac376d33b9f7af230f
K1rLes <kirill.lesovoi@mail.ru>
2023-03-27 04:11:39 +0500

    v 1.5.0.2
    Из-за точки в сообщении при запуске среда не могла понять формат логов xD

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     import create_app
     app = create_app.init_app()
     host, port = '0.0.0.0', 5000
-    logger.info(f'Running a Flask "Simple-Chess-API" on {host}:{port}.')
+    logger.info(f'Running a Flask "Simple-Chess-API" on {host}:{port}')
     serve(app, host=host, port=port)
 
 
